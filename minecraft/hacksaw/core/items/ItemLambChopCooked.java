26b0079ebb58562d86fb4eadcebe93972faddb4b
DV8FromTheWorld <keeneraustin@yahoo.com>
2012-08-05 00:05:30 -0400

    updated the food.png and the food classes to match it

--- minecraft/hacksaw/core/items/ItemLambChopCooked.java
+++ minecraft/hacksaw/core/items/ItemLambChopCooked.java
@@ -14,7 +14,7 @@ public class ItemLambChopCooked extends ItemFood{
 	public ItemLambChopCooked(int par1, int par2, float par3, boolean par4) {
 		super(par1, par2, par3, par4);
 		this.setMaxStackSize(64);
-		this.setIconCoord(0, 0);
+		this.setIconCoord(4, 0);
 		this.setItemName("lamb.chop.cooked");
 		par2 = foodHealAmount;
 		par3 = saturationAmount;
--- minecraft/hacksaw/core/items/ItemLambChopRaw.java
+++ minecraft/hacksaw/core/items/ItemLambChopRaw.java
@@ -15,7 +15,7 @@ public class ItemLambChopRaw extends ItemFood{
 	public ItemLambChopRaw(int par1, int par2, float par3, boolean par4) {
 		super(par1, par2, par3, par4);
 		this.setMaxStackSize(64);
-		this.setIconCoord(0, 0);
+		this.setIconCoord(2, 0);
 		this.setItemName("lamb.chop.raw");
 		par2 = foodHealAmount;
 		par3 = saturationAmount;
