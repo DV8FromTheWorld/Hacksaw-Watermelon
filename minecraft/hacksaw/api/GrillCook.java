d86a06295c4fb0afb5523fdd1a95358f4d7d38f0
DV8FromTheWorld <keeneraustin@yahoo.com>
2012-08-04 22:55:21 -0400

    First file of the API.  Use for the Grill

---
+++ minecraft/hacksaw/api/GrillCook.java
@@ -0,0 +1,67 @@
+package hacksaw.api;
+
+import hacksaw.core.machines.GrillRecipes;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
+import net.minecraft.src.ItemStack;
+
+
+
+public class GrillCook {
+	
+	private static GrillCook INSTANCE = new GrillCook();
+		
+	private GrillCook(){
+		
+	}
+	
+	
+	/*
+	 * Use this to access methods within this API class
+	 * Ex: GrillCook.getInstance().addToGrillToCook	 * 
+	 */
+	public static GrillCook getInstance(){
+		return INSTANCE;		
+	}
+	
+	/*
+	 * Use this method to be able to cook something on the grill.  
+	 * This uses the default "cooking" time.
+	 * 
+	 * @param input: ID of the item to be "cooked" (Note, this needs to be the shiftedIndex ID)
+	 * @param output: the "cooked" version of the item
+	 */
+	public void addToGrillToCook(int input, ItemStack output){
+		try{
+			
+		}catch(Exception exception){
+			
+		}
+	}
+	
+	
+	/*
+	 * Use this method to be able to cook something on the grill.  
+	 * This method allows specifying an amount of time to cook the item.
+	 * 
+	 * @param input: ID of the item to be "cooked" (Note, this needs to be the shiftedIndex ID)
+	 * @param output: the "cooked" version of the item
+	 * @param cookTime: amount of time needed to cook the item (in seconds)
+	 */
+	public void addToGrillToCook(int input, ItemStack output, int cookTime){
+		try{	
+			Class GrillCookRecipes = Class.forName("hacksaw.core.machines.GrillRecipes");
+			Method bob = GrillCookRecipes.getMethod("addToGrillToCook", new Class[]{ Integer.TYPE, ItemStack.class, Integer.TYPE});
+			bob.invoke(null, new Object[]{
+					input, output, cookTime
+			});
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+}
