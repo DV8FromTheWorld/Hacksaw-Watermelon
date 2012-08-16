14e3d0a84b0f16983ed0f076dd50fea888b43663
DV8FromTheWorld <keeneraustin@yahoo.com>
2012-08-16 02:34:51 -0400

    Added ItemBaseFood

---
+++ Hacksaw-Common/hacksaw/core/items/ItemBaseFood.java
@@ -0,0 +1,25 @@
+package hacksaw.core.items;
+
+import net.minecraft.src.EnumAction;
+import net.minecraft.src.ItemFood;
+import net.minecraft.src.ItemStack;
+
+public abstract class ItemBaseFood extends ItemFood{
+		
+	public ItemBaseFood(int par1, int foodHealAmount, float saturationAmount, boolean par4) {
+		super(par1, foodHealAmount, saturationAmount, par4);
+	
+	}	
+	public ItemBaseFood(int par1, int foodHealAmount, boolean par3) {
+		super(par1, foodHealAmount, par3);
+	}
+	
+	public abstract EnumAction getItemUseAction(ItemStack par1ItemStack);
+		
+	public abstract String getTextureFile();
+	
+	public abstract int getHealAmount();
+	
+	public abstract float getSaturationModifier();
+	
+}
