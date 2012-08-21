6bf7faccb0819821d589cdb6bae758178dab50a1
DV8FromTheWorld <keeneraustin@yahoo.com>
2012-08-20 23:44:10 -0400

    Done testing.

--- Hacksaw-Common/hacksaw/core/items/ItemChefKnifeSharp.java
+++ Hacksaw-Common/hacksaw/core/items/ItemChefKnifeSharp.java
@@ -5,7 +5,7 @@ import net.minecraft.src.Item;
 import net.minecraft.src.ItemStack;
 import net.minecraft.src.World;
 
-public class ItemChefKnifeSharp extends Item {
+public class ItemChefKnifeSharp extends ItemBaseChefTools {
 	
 	public ItemChefKnifeSharp(int i) {
 		super(i);
@@ -19,19 +19,6 @@ public class ItemChefKnifeSharp extends Item {
 	public Item getContainerItem(){
 		return this;
 	}
-	
-	public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7)
-    {
-		System.out.println("ItemStack: " +String.valueOf(par1ItemStack));
-		System.out.println("EntityPlayer: " +String.valueOf(par2EntityPlayer));
-		System.out.println("world: " +String.valueOf(par3World));
-		System.out.println("int par4: " +String.valueOf(par4));
-		System.out.println("int par5: " +String.valueOf(par5));
-		System.out.println("int par6: " +String.valueOf(par6));
-		System.out.println("int par7: " +String.valueOf(par7));
-		par1ItemStack.stackSize--;
-        return true;
-    }
 
 	@Override
 	public boolean shouldRotateAroundWhenRendering() {
