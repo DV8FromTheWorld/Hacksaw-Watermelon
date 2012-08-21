e92add2df670d0527fc9e4ae6ebcf16078cf697f
DV8FromTheWorld <keeneraustin@yahoo.com>
2012-08-20 22:08:08 -0400

    Changes to ItemBaseSeed, now ItemCropSeed

--- Hacksaw-Common/hacksaw/core/items/ItemCropSeed.java
+++ Hacksaw-Common/hacksaw/core/items/ItemCropSeed.java
@@ -2,14 +2,14 @@ package hacksaw.core.items;
 
 import java.util.ArrayList;
 
-import net.minecraft.src.ItemSeeds;
+import net.minecraft.src.Item;
 import net.minecraft.src.ItemStack;
 import net.minecraft.src.forge.ITextureProvider;
 
-public class ItemBaseSeed extends ItemSeeds implements ITextureProvider{
+public abstract class ItemCropSeed extends Item implements ITextureProvider{
 
-	public ItemBaseSeed(int par1, int par2, int par3) {
-		super(par1, par2, par3);
+	public ItemCropSeed(int par1) {
+		super(par1);
 		
 	}
 	
