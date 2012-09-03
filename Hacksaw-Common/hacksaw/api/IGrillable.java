32b1171242bad3dafebe143389a2627016c4a5b0
DV8FromTheWorld <keeneraustin@yahoo.com>
2012-09-03 15:01:40 -0400

    messing around with the IGrillable interface

--- Hacksaw-Common/hacksaw/api/IGrillable.java
+++ Hacksaw-Common/hacksaw/api/IGrillable.java
@@ -2,4 +2,21 @@ package hacksaw.api;
 
 public interface IGrillable {
 
+	
+	/**
+	 * Needs to return the amount to time that the food should be cooked
+	 * Return a time in seconds
+	 * @return
+	 */
+	int getCookingTime();
+	
+	
+	/**
+	 * Return the shifted index of the cooked food
+	 * @return
+	 */
+	int getCookedItemId();
+	
+	
+	
 }
