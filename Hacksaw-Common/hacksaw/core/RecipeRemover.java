2ee22f4c907cb6f62cf128fae24cabbc4b12d0e7
Eurymachus <reflex_ion@hotmail.com>
2012-09-16 01:38:41 +0100

    Modified RecipeRemover to be more flexible

--- Hacksaw-Common/hacksaw/core/RecipeRemover.java
+++ Hacksaw-Common/hacksaw/core/RecipeRemover.java
@@ -10,25 +10,40 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import net.minecraft.src.Block;
 import net.minecraft.src.CraftingManager;
 import net.minecraft.src.IRecipe;
 import net.minecraft.src.Item;
 import net.minecraft.src.ItemStack;
+import net.minecraft.src.ModLoader;
 
 public class RecipeRemover {
 	
-	public static void removeVanillaRecipes(){
-		if(CoreConfiguration.getPreference(CoreConfiguration.USE_VANILLA_RECIPES) == false){
-			HacksawLogger.log(LogLevel.DEBUG, "Removing vanilla recipes");
-			removeCrafting();
-			removeSmelting();
-		}	
+	// set of recipes to remove
+	private static Set<Integer> recipeSet = new HashSet<Integer>();
+	
+	public static void registerItemRecipeToRemove(Object object) {
+		if (object instanceof Block) {
+			recipeSet.add(((Block)object).blockID);
+		}
+		if (object instanceof Item) {
+			recipeSet.add(((Item)object).shiftedIndex);
+		}
 	}
-
+	
+	// set of recipes to remove
+	private static Set<Integer> smeltingSet = new HashSet<Integer>();
+	
+	public static void registerItemSmeltingToRemove(Object object) {
+		if (object instanceof Block) {
+			smeltingSet.add(((Block)object).blockID);
+		}
+		if (object instanceof Item) {
+			smeltingSet.add(((Item)object).shiftedIndex);
+		}
+	}
+	
 	private static void removeCrafting() {
-		// set of recipes to remove
-		Set<Integer> itemSet = new HashSet<Integer>();
-		itemSet.add(Item.bread.shiftedIndex);
 		
 		// remove everything from the list
 		List<IRecipe> recipes = (List)CraftingManager.getInstance().getRecipeList();
@@ -37,18 +52,28 @@ public class RecipeRemover {
 		while( it.hasNext() ) {
 			IRecipe recipe = it.next();
 			ItemStack output = recipe.getRecipeOutput();
-			if( itemSet.contains(output.itemID)) {
+			if( recipeSet.contains(output.itemID)) {
 				matches.add(recipe);
 			}
 		}
 		for( IRecipe recipe : matches ) {
-			HacksawLogger.log(LogLevel.DEBUG, "Removing recipe for "+recipe);
+			ModLoader.getLogger().info("Removing recipe for "+recipe);
 			recipes.remove(recipe);
 		}
 	}
-		
+	
 	private static void removeSmelting() {
 		// TODO: repeat above but with FurnaceRecipes
 	}
 	
+	public static void removeVanillaRecipes(){
+		if(CoreConfiguration.getPreference(CoreConfiguration.USE_VANILLA_RECIPES) == false){
+			HacksawLogger.log(LogLevel.DEBUG, "Registering recipes to remove");
+			registerItemRecipeToRemove(Item.bread.shiftedIndex);
+			
+			HacksawLogger.log(LogLevel.DEBUG, "Removing vanilla recipes");
+			removeCrafting();
+			removeSmelting();
+		}	
+	}
 }
