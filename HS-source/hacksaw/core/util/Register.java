b1a548dcba0b0713ee0a70112f58efdbb5497951
DV8FromTheWorld <keeneraustin@yahoo.com>
2014-01-29 03:38:42 -0500

    Fixed knife recipes not working after the knife had been damaged from 1 craft. Added ChefKnifeRecipe, fixes bug mentioned above. Added new package:  hacksaw.recipes   Contains new class listed above. Also, fixed Eclipse java formatter, so will be cleaning up the syntax formatting over the next few commits.

--- HS-source/hacksaw/core/util/Register.java
+++ HS-source/hacksaw/core/util/Register.java
@@ -29,185 +29,198 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import recipes.ChefKnifeRecipe;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-public class Register {
-	
-	public static void registerItemsAndBlocksAndRecipes(){
-		registerBlocks();
-		registerBlockNames();
-		registerItems();
-		registerItemNames();
-		registerHandlers();
-		addShapedRecipes();
-		addShapelessRecipes();
-		addGrillRecipes();
-		addJuicerRecipes();
-	}
+public class Register
+{
 
-	private static void registerBlocks() {
-		//====================== Registers blocks ========================
-		
-		//---------Vegetation----------
-			// if the block was removed
-			if (BlockRemover.removeVanillaBlock(Block.melon)) {
-				// initialize the block
-				HacksawBlocks.supermelon.block = new BlockSuperMelon(HacksawBlocks.supermelon.blockId);
-				// log some success message
-				HacksawLogger.log(LogLevel.DEBUG, "BlockSuperMelon loaded successfully.");
-			}
+    public static void registerItemsAndBlocksAndRecipes()
+    {
+        registerBlocks();
+        registerBlockNames();
+        registerItems();
+        registerItemNames();
+        registerHandlers();
+        addShapedRecipes();
+        addShapelessRecipes();
+        addGrillRecipes();
+        addJuicerRecipes();
+    }
 
-			HacksawBlocks.carrotCrop.block = new BlockCarrot(HacksawBlocks.carrotCrop.blockId);
-			HacksawBlocks.lettuceCrop.block = new BlockLettuce(HacksawBlocks.lettuceCrop.blockId);
-/** PLACEHOLDERS
-			HacksawBlocks.grill.block = new BlockGrill(HacksawBlocks.grill.blockId, TileEntityGrill.class, Material.iron);
-			HacksawBlocks.grillElectric.block = new BlockGrill(HacksawBlocks.grillElectric.blockId, TileEntityGrillElectric.class, Material.iron);
-**/
-		//========= Register Each Block ==============
-			
-			// for each Block within the enum (HacksawBlocks)
-			for (HacksawBlocks blocks : HacksawBlocks.values()) {
-				// Check the block is initialized
-				if (blocks.block != null) {
-					// Register the block with ModLoader
-					GameRegistry.registerBlock(blocks.block);
-				}
-			}
-	}
+    private static void registerBlocks()
+    {
+        //====================== Registers blocks ========================
 
-	private static void registerBlockNames() {
-		//==================== Registers the names for the blocks ======================
-		
-			// for each Block within the enum (HacksawBlocks)
-			for (HacksawBlocks blocks : HacksawBlocks.values()) {
-				// Check that the block is initialized and the block name is set
-				if (blocks.block != null && blocks.name != null) {
-					// Register the name of the block with ModLoader
-					LanguageRegistry.addName(blocks.block, blocks.name);
-				}
-			}
-	}
+        //---------Vegetation----------
+        // if the block was removed
+        if (BlockRemover.removeVanillaBlock(Block.melon))
+        {
+            // initialize the block
+            HacksawBlocks.supermelon.block = new BlockSuperMelon(HacksawBlocks.supermelon.blockId);
+            // log some success message
+            HacksawLogger.log(LogLevel.DEBUG, "BlockSuperMelon loaded successfully.");
+        }
 
-	private static void registerItems() {
-		//====================== Registers the Items ========================
-		
-		//---------Tools----------
-			HacksawItems.chefKnifeSharp.item = new ItemChefKnifeSharp(HacksawItems.chefKnifeSharp.itemId);
-			HacksawItems.chefKnifeDull.item = new ItemChefKnifeDull(HacksawItems.chefKnifeDull.itemId);
-			HacksawItems.knifeSharpener.item = new ItemKnifeSharpener(HacksawItems.knifeSharpener.itemId);
-		
-		//---------Food-----------		
-			HacksawItems.carrot.item = new ItemCarrot(HacksawItems.carrot.itemId, 2, 0.3F, false);
-			HacksawItems.carrotSliced.item = new ItemCarrotSliced(HacksawItems.carrotSliced.itemId, 1, 0.15F, false);
-			HacksawItems.lettuce.item = new ItemLettuce(HacksawItems.lettuce.itemId, 2, 0.3F, false);
-			HacksawItems.lettuceSliced.item = new ItemLettuceSliced(HacksawItems.lettuceSliced.itemId, 1, 0.15F, false);
-			HacksawItems.lambChopRaw.item = new ItemLambChopRaw(HacksawItems.lambChopRaw.itemId, 3, 0.3F, false);
-			HacksawItems.lambChopCooked.item = new ItemLambChopCooked(HacksawItems.lambChopCooked.itemId, 7, 0.7F, false);
-			HacksawItems.orange.item = new ItemOrange(HacksawItems.orange.itemId, 4, 0.3F, false);
-			HacksawItems.orangeSliced.item = new ItemOrangeSliced(HacksawItems.orangeSliced.itemId, 2, 0.15F, false);
-			HacksawItems.appleRedSliced.item = new ItemAppleRedSliced(HacksawItems.appleRedSliced.itemId, 2, 0.15F, false);
-			HacksawItems.appleGreen.item = new ItemAppleGreen(HacksawItems.appleGreen.itemId, 4, 0.3F, false);
-			HacksawItems.appleGreenSliced.item = new ItemAppleGreenSliced(HacksawItems.appleGreenSliced.itemId, 2, 0.15F, false);
-			
-			
-		//---------Seeds----------
-			HacksawItems.carrotSeed.item = new ItemCarrotSeed(HacksawItems.carrotSeed.itemId, HacksawBlocks.carrotCrop.blockId);
-			HacksawItems.lettuceSeed.item = new ItemLettuceSeed(HacksawItems.lettuceSeed.itemId, HacksawBlocks.lettuceCrop.blockId);
-			//HacksawItems.appleSeed.item = new ItemAppleSeed(HacksawItems.appleSeed.itemId, HacksawBlocks.appleSappling.blockId);
-	}
+        HacksawBlocks.carrotCrop.block = new BlockCarrot(HacksawBlocks.carrotCrop.blockId);
+        HacksawBlocks.lettuceCrop.block = new BlockLettuce(HacksawBlocks.lettuceCrop.blockId);
+        /**
+         * PLACEHOLDERS
+         * HacksawBlocks.grill.block = new
+         * BlockGrill(HacksawBlocks.grill.blockId, TileEntityGrill.class,
+         * Material.iron);
+         * HacksawBlocks.grillElectric.block = new
+         * BlockGrill(HacksawBlocks.grillElectric.blockId,
+         * TileEntityGrillElectric.class, Material.iron);
+         **/
+        //========= Register Each Block ==============
 
-	private static void registerItemNames() {
-		//======================= Registers the names for the items =======================
-		
-			// for each Item within the enum (HacksawItems)
-			for (HacksawItems items : HacksawItems.values()) {
-				// Check that the item is initialized and the item name is set
-				if (items.item != null & items.name != null) {
-					// Register the name of the item with ModLoader
-					LanguageRegistry.addName(items.item, items.name);
-				}
-			}
-	}
-	
-	private static void addShapedRecipes() {
-		//==================== Registers the shaped-recipes ====================
-		
-		//recipe for creating the Chef knife.  Contains full durability, so it is the "Sharp Chef Knife"
-			GameRegistry.addRecipe(new ItemStack(HacksawItems.chefKnifeSharp.item, 1), new Object[]{
-				"  Y", " XY", "XY ", Character.valueOf('X'), Item.stick, Character.valueOf('Y'), Item.ingotIron
-			});
-		
-		//recipe for creating a Knife Sharpener
-			GameRegistry.addRecipe(new ItemStack(HacksawItems.knifeSharpener.item, 1), new Object[]{
-				"X X", "YXY", "ZZZ", Character.valueOf('X'), Item.flint, Character.valueOf('Y'), Block.cobblestone, Character.valueOf('Z'), Block.planks
-			});	
-	}
-	
-	private static void addShapelessRecipes() {
-		//=================== Registers the shapeless-recipes ====================
-			
-		//recipe for using the Chef knife to cut melon blocks to get melon slices
-			GameRegistry.addShapelessRecipe(new ItemStack(Item.melon, 4), new Object[]{
-				HacksawBlocks.supermelon.block, new ItemStack(HacksawItems.chefKnifeSharp.item, 1, -1)//Note: a new itemstack is made here because you want to be able to use the item no matter what the damage value
-			});
-		
-		//recipe for "sharpening" the Dull Chef knife
-			GameRegistry.addShapelessRecipe(new ItemStack(HacksawItems.chefKnifeSharp.item), new Object[]{
-				new ItemStack(HacksawItems.chefKnifeDull.item, 1, -1), new ItemStack(HacksawItems.knifeSharpener.item, 1, -1)
-			});
-			
-		//carrot seeds come from carrots
-			GameRegistry.addShapelessRecipe(new ItemStack(HacksawItems.carrotSeed.item), new Object[]{
-				new ItemStack(HacksawItems.carrot.item, 1, -1)
-			});
-			
-		//lettuce seeds come from lettuce
-			GameRegistry.addShapelessRecipe(new ItemStack(HacksawItems.lettuceSeed.item), new Object[]{
-				new ItemStack(HacksawItems.lettuce.item, 1, -1)
-			});
-			
-		//recipe for sliced carrots
-			GameRegistry.addShapelessRecipe(new ItemStack(HacksawItems.carrotSliced.item, 2), new Object[]{
-				new ItemStack(HacksawItems.chefKnifeSharp.item, 1, -1), HacksawItems.carrot.item 
-			});
-			
-		//recipe for sliced lettuce
-			GameRegistry.addShapelessRecipe(new ItemStack(HacksawItems.lettuceSliced.item, 4), new Object[]{
-				new ItemStack(HacksawItems.chefKnifeSharp.item, 1, -1), HacksawItems.lettuce.item 
-			});
-			
-		//recipe for sliced orange
-			GameRegistry.addShapelessRecipe(new ItemStack(HacksawItems.orangeSliced.item, 2), new Object[]{
-				new ItemStack(HacksawItems.chefKnifeSharp.item, 1, -1), HacksawItems.orange.item
-			});
-			
-		//recipe for sliced apple
-			GameRegistry.addShapelessRecipe(new ItemStack(HacksawItems.appleRedSliced.item, 2), new Object[]{
-				new ItemStack(HacksawItems.chefKnifeSharp.item, 1, -1), Item.appleRed 
-			});
-			
-		//recipe for the sliced green apple
-			GameRegistry.addShapelessRecipe(new ItemStack(HacksawItems.appleGreenSliced.item, 2), new Object[]{
-				new ItemStack(HacksawItems.chefKnifeSharp.item, 1, -1), HacksawItems.appleGreen.item
-			});
-	}
-	
-	private static void registerHandlers(){
-		//================= Registers Forge Handlers =========================
-		MinecraftForge.EVENT_BUS.register(new MeatProvider());
-		
-	}
-	
-	private static void addGrillRecipes(){
-		//================== Registers the cooking recipes for the Grill ===================
-		
-		GrillRecipes.addToGrillToCook(HacksawItems.lambChopRaw.item.itemID, new ItemStack(HacksawItems.lambChopCooked.item), 25);
-	}
-	
-	private static void addJuicerRecipes(){
-		//================== Registers the juicing recipes for the Juicer ====================
-		
-		
-	}
+        // for each Block within the enum (HacksawBlocks)
+        for (HacksawBlocks blocks : HacksawBlocks.values())
+        {
+            // Check the block is initialized
+            if (blocks.block != null)
+            {
+                // Register the block with ModLoader
+                //TODO: Before updating to beyond 1.6.4, change this. 
+                GameRegistry.registerBlock(blocks.block);
+            }
+        }
+    }
+
+    private static void registerBlockNames()
+    {
+        //==================== Registers the names for the blocks ======================
+
+        // for each Block within the enum (HacksawBlocks)
+        for (HacksawBlocks blocks : HacksawBlocks.values())
+        {
+            // Check that the block is initialized and the block name is set
+            if (blocks.block != null && blocks.name != null)
+            {
+                // Register the name of the block with ModLoader
+                LanguageRegistry.addName(blocks.block, blocks.name);
+            }
+        }
+    }
+
+    private static void registerItems()
+    {
+        //====================== Registers the Items ========================
+
+        //---------Tools----------
+        HacksawItems.chefKnifeSharp.item = new ItemChefKnifeSharp(HacksawItems.chefKnifeSharp.itemId);
+        HacksawItems.chefKnifeDull.item = new ItemChefKnifeDull(HacksawItems.chefKnifeDull.itemId);
+        HacksawItems.knifeSharpener.item = new ItemKnifeSharpener(HacksawItems.knifeSharpener.itemId);
+
+        //---------Food-----------		
+        HacksawItems.carrot.item = new ItemCarrot(HacksawItems.carrot.itemId, 2, 0.3F, false);
+        HacksawItems.carrotSliced.item = new ItemCarrotSliced(HacksawItems.carrotSliced.itemId, 1, 0.15F, false);
+        HacksawItems.lettuce.item = new ItemLettuce(HacksawItems.lettuce.itemId, 2, 0.3F, false);
+        HacksawItems.lettuceSliced.item = new ItemLettuceSliced(HacksawItems.lettuceSliced.itemId, 1, 0.15F, false);
+        HacksawItems.lambChopRaw.item = new ItemLambChopRaw(HacksawItems.lambChopRaw.itemId, 3, 0.3F, false);
+        HacksawItems.lambChopCooked.item = new ItemLambChopCooked(HacksawItems.lambChopCooked.itemId, 7, 0.7F, false);
+        HacksawItems.orange.item = new ItemOrange(HacksawItems.orange.itemId, 4, 0.3F, false);
+        HacksawItems.orangeSliced.item = new ItemOrangeSliced(HacksawItems.orangeSliced.itemId, 2, 0.15F, false);
+        HacksawItems.appleRedSliced.item = new ItemAppleRedSliced(HacksawItems.appleRedSliced.itemId, 2, 0.15F, false);
+        HacksawItems.appleGreen.item = new ItemAppleGreen(HacksawItems.appleGreen.itemId, 4, 0.3F, false);
+        HacksawItems.appleGreenSliced.item = new ItemAppleGreenSliced(HacksawItems.appleGreenSliced.itemId, 2, 0.15F, false);
+
+        //---------Seeds----------
+        HacksawItems.carrotSeed.item = new ItemCarrotSeed(HacksawItems.carrotSeed.itemId, HacksawBlocks.carrotCrop.blockId);
+        HacksawItems.lettuceSeed.item = new ItemLettuceSeed(HacksawItems.lettuceSeed.itemId, HacksawBlocks.lettuceCrop.blockId);
+        //HacksawItems.appleSeed.item = new ItemAppleSeed(HacksawItems.appleSeed.itemId, HacksawBlocks.appleSappling.blockId);
+    }
+
+    private static void registerItemNames()
+    {
+        //======================= Registers the names for the items =======================
+
+        // for each Item within the enum (HacksawItems)
+        for (HacksawItems items : HacksawItems.values())
+        {
+            // Check that the item is initialized and the item name is set
+            if (items.item != null & items.name != null)
+            {
+                // Register the name of the item with ModLoader
+                LanguageRegistry.addName(items.item, items.name);
+            }
+        }
+    }
+
+    private static void addShapedRecipes()
+    {
+        //==================== Registers the shaped-recipes ====================
+
+        //recipe for creating the Chef knife.  Contains full durability, so it is the "Sharp Chef Knife"
+        GameRegistry.addRecipe(new ItemStack(HacksawItems.chefKnifeSharp.item, 1), new Object[] { "  Y", " XY", "XY ", Character.valueOf('X'),
+                Item.stick, Character.valueOf('Y'), Item.ingotIron });
+
+        //recipe for creating a Knife Sharpener
+        GameRegistry.addRecipe(new ItemStack(HacksawItems.knifeSharpener.item, 1), new Object[] { "X X", "YXY", "ZZZ", Character.valueOf('X'),
+                Item.flint, Character.valueOf('Y'), Block.cobblestone, Character.valueOf('Z'), Block.planks });
+    }
+
+    private static void addShapelessRecipes()
+    {
+        //=================== Registers the shapeless-recipes ====================
+
+        //recipe for using the Chef knife to cut melon blocks to get melon slices
+        GameRegistry.addRecipe(new ChefKnifeRecipe(new ItemStack(Item.melon, 4), new Object[] { HacksawBlocks.supermelon.block,
+                new ItemStack(HacksawItems.chefKnifeSharp.item, 1, -1) //Note: a new itemstack is made here because you want to be able to use the item no matter what the damage value
+                }));
+
+        //recipe for "sharpening" the Dull Chef knife
+        GameRegistry.addShapelessRecipe(new ItemStack(HacksawItems.chefKnifeSharp.item), new Object[] { HacksawItems.chefKnifeDull.item,
+                HacksawItems.knifeSharpener.item });
+
+        //carrot seeds come from carrots
+        GameRegistry.addRecipe(new ChefKnifeRecipe(new ItemStack(HacksawItems.carrotSeed.item), new Object[] { new ItemStack(
+                HacksawItems.carrot.item, 1, -1) }));
+
+        //lettuce seeds come from lettuce
+        GameRegistry.addRecipe(new ChefKnifeRecipe(new ItemStack(HacksawItems.lettuceSeed.item), new Object[] { new ItemStack(
+                HacksawItems.lettuce.item, 1, -1) }));
+
+        //recipe for sliced carrots
+        GameRegistry.addRecipe(new ChefKnifeRecipe(new ItemStack(HacksawItems.carrotSliced.item, 2), new Object[] {
+                new ItemStack(HacksawItems.chefKnifeSharp.item, 1, -1), HacksawItems.carrot.item }));
+
+        //recipe for sliced lettuce
+        GameRegistry.addRecipe(new ChefKnifeRecipe(new ItemStack(HacksawItems.lettuceSliced.item, 4), new Object[] {
+                new ItemStack(HacksawItems.chefKnifeSharp.item, 1, -1), HacksawItems.lettuce.item }));
+
+        //recipe for sliced orange
+        GameRegistry.addRecipe(new ChefKnifeRecipe(new ItemStack(HacksawItems.orangeSliced.item, 2), new Object[] {
+                new ItemStack(HacksawItems.chefKnifeSharp.item, 1, -1), HacksawItems.orange.item }));
+
+        //recipe for sliced apple
+        GameRegistry.addRecipe(new ChefKnifeRecipe(new ItemStack(HacksawItems.appleRedSliced.item, 2), new Object[] {
+                new ItemStack(HacksawItems.chefKnifeSharp.item, 1, -1), Item.appleRed }));
+
+        //recipe for the sliced green apple
+        GameRegistry.addRecipe(new ChefKnifeRecipe(new ItemStack(HacksawItems.appleGreenSliced.item, 2), new Object[] {
+                new ItemStack(HacksawItems.chefKnifeSharp.item, 1, -1), HacksawItems.appleGreen.item }));
+    }
+
+    private static void registerHandlers()
+    {
+        //================= Registers Forge Handlers =========================
+        MinecraftForge.EVENT_BUS.register(new MeatProvider());
+
+    }
+
+    private static void addGrillRecipes()
+    {
+        //================== Registers the cooking recipes for the Grill ===================
+
+        GrillRecipes.addToGrillToCook(HacksawItems.lambChopRaw.item.itemID, new ItemStack(HacksawItems.lambChopCooked.item), 25);
+    }
+
+    private static void addJuicerRecipes()
+    {
+        //================== Registers the juicing recipes for the Juicer ====================
+
+    }
 }
---
+++ HS-source/recipes/ChefKnifeRecipe.java
@@ -0,0 +1,176 @@
+package recipes;
+
+import hacksaw.core.HacksawItems;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import net.minecraft.block.Block;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.world.World;
+
+/**
+ * Provides shapeless crafting for the ChefKnife.
+ * Required because Minecraft doesn't support damaged items correctly.
+ * 
+ * @author DV8FromTheWorld (Austin Keener)
+ */
+public class ChefKnifeRecipe implements IRecipe
+{
+
+    /** Is the ItemStack that you get when craft the recipe. */
+    private final ItemStack recipeOutput;
+
+    /** Is a List of ItemStack that composes the recipe. */
+    public final ArrayList<ItemStack> recipeItems;
+
+    /**
+     * Constructor to create a recipe that doesn't ignores the Chef Knife's
+     * damage value.
+     * 
+     * @param itemStackOutput
+     *            The output of the recipe.
+     * @param inputRecipe
+     *            The recipe needed to create the output ItemStack.
+     */
+    public ChefKnifeRecipe(ItemStack itemStackOutput, Object... inputRecipe)
+    {
+        this.recipeOutput = itemStackOutput;
+        this.recipeItems = recipeAsArrayList(inputRecipe);
+    }
+
+    /**
+     * Returns the output of this recipe as an ItemStack.
+     * 
+     * @return The recipe output.
+     */
+    @Override
+    public ItemStack getRecipeOutput()
+    {
+        return this.recipeOutput;
+    }
+
+    /**
+     * Used to check if a recipe matches current crafting inventory.
+     * Different from vanilla one in that it doesn't compare damage value of the
+     * chef knife.
+     * 
+     * @param inventoryCrafting
+     *            The inventory of the crafting table being used by the player.
+     * @param world
+     *            The current world that player is in.
+     */
+    @Override
+    public boolean matches(InventoryCrafting inventoryCrafting, World world)
+    {
+        ArrayList<ItemStack> recipeList = new ArrayList<ItemStack>(this.recipeItems);
+
+        for (int i = 0; i < 3; ++i)
+        {
+            for (int j = 0; j < 3; ++j)
+            {
+                ItemStack itemInCraftingTable = inventoryCrafting.getStackInRowAndColumn(j, i);
+
+                if (itemInCraftingTable != null)
+                {
+                    boolean flag = false;
+                    Iterator<ItemStack> itemInRecipe = recipeList.iterator();
+
+                    while (itemInRecipe.hasNext())
+                    {
+                        ItemStack itemstack1 = itemInRecipe.next();
+
+                        if (itemInCraftingTable.itemID == itemstack1.itemID)
+                        {
+                            if (itemInCraftingTable.itemID == HacksawItems.chefKnifeSharp.item.itemID)
+                            {
+                                flag = true;
+                                recipeList.remove(itemstack1);
+                                break;
+                            }
+                            else if (itemstack1.getItemDamage() == 32767 || itemInCraftingTable.getItemDamage() == itemstack1.getItemDamage())
+                            {
+                                flag = true;
+                                recipeList.remove(itemstack1);
+                                break;
+                            }
+
+                        }
+                    }
+
+                    if (!flag)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return recipeList.isEmpty();
+    }
+
+    /**
+     * Returns an Item that is the result of this recipe
+     * 
+     * @param inventoryCrafting
+     *            The inventory of the crafting table being used by the player.
+     */
+    @Override
+    public ItemStack getCraftingResult(InventoryCrafting inventoryCrafting)
+    {
+        return this.recipeOutput.copy();
+    }
+
+    /**
+     * Returns the size of the recipe area
+     * 
+     * @return returns the total size of the recipe (max 9).
+     */
+    @Override
+    public int getRecipeSize()
+    {
+        return this.recipeItems.size();
+    }
+
+    /**
+     * Helper method that takes apart the recipe and puts it into a list.
+     * Essentially identical to the implementation of
+     * CraftingManager.addShapelessRecipe(ItemStack, Object...).
+     * 
+     * @param recipe
+     *            The recipe, as an Object[], to be changed to an arrayList.
+     * @return
+     */
+    private ArrayList<ItemStack> recipeAsArrayList(Object[] recipe)
+    {
+        ArrayList<ItemStack> recipeList = new ArrayList<ItemStack>();
+        int i = recipe.length;
+
+        for (int j = 0; j < i; ++j)
+        {
+            Object partOfRecipe = recipe[j];
+
+            if (partOfRecipe instanceof ItemStack)
+            {
+                recipeList.add(((ItemStack) partOfRecipe).copy());
+            }
+            else if (partOfRecipe instanceof Item)
+            {
+                recipeList.add(new ItemStack((Item) partOfRecipe));
+            }
+            else
+            {
+                if (!(partOfRecipe instanceof Block))
+                {
+                    throw new RuntimeException("Invalid ChefKnifeRecipe!  Attemped to add: " + partOfRecipe + " to recipe");
+                }
+
+                recipeList.add(new ItemStack((Block) partOfRecipe));
+            }
+        }
+        return recipeList;
+    }
+}
