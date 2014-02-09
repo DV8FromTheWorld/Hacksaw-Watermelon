0d4365b051e2d14ceeb25d2ba4f25f453a69c9d9
Austin Keener <keeneraustin@yahoo.com>
2014-02-09 05:22:58 -0500

    Created Coord class as a helper class for defining coordinates on a GUI.

---
+++ HS-source/hacksaw/core/containers/Coord.java
@@ -0,0 +1,23 @@
+package hacksaw.core.containers;
+
+/**
+ * Defines a Coordinate class.
+ * Stores X and Y coordinates for GUI use.
+ */
+public class Coord
+{
+    public int Y;
+    public int X;
+    
+    /**
+     * Creates a Coordinate with specified values.
+     * 
+     * @param x The x portion of the Coord.
+     * @param y The y portion of the Coord.
+     */
+    public Coord(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
